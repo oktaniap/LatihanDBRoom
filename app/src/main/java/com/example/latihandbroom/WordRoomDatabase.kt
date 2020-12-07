da49eaa9df6066a6e4dd133b1aba200fb68be5b4
Oktania Purwaningrum <oktaniap@gmail.com>
2020-12-07 09:56:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihandbroom/WordRoomDatabase.kt
@@ -0,0 +1,60 @@
+package com.example.latihandbroom
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+//Annotates class to be a Room Database with a table (entity) of the Word Class
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope) : RoomDatabase.Callback() {
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+            // TODO: Add your own words!
+        }
+    }
+
+    companion object{
+        @Volatile
+        private var INSTANCE: WordRoomDatabase?=null
+        fun getDatabase(
+            context: Context,
+            viewModelScope: CoroutineScope
+        ): WordRoomDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
